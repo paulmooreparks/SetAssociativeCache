3feed46a5f028c92faafc670460741d71e35b72e
Paul M. Parks <paul@smartsam.com>
2021-07-02 16:31:25 +0800

    LRU is mostly implemented. LFU is a complete copy of it; need to update LFU with the necessary logic to implement the policy correctly.

--- ArrayCacheImplBase.cs
+++ ArrayCacheImplBase.cs
@@ -9,13 +9,10 @@ namespace ParksComputing.SetAssociativeCache {
         public ArrayCacheImplBase(int sets, int ways) {
             Sets = sets;
             Ways = ways;
-            itemArray = new KeyValuePair<TKey, TValue>[Capacity];
-            indexArray = new int[Capacity];
-            Array.Fill(indexArray, int.MaxValue);
+            ItemArray = new KeyValuePair<TKey, TValue>[Capacity];
         }
 
-        protected KeyValuePair<TKey, TValue>[] itemArray;
-        protected int[] indexArray;
+        protected KeyValuePair<TKey, TValue>[] ItemArray { get; set; }
         public int Sets { get; protected set; }
         public int Ways { get; protected set; }
 
@@ -35,19 +32,7 @@ namespace ParksComputing.SetAssociativeCache {
 
         public int Capacity { get => Sets * Ways; }
 
-        public int Count {
-            get {
-                int value = 0;
-
-                foreach (int itemIndex in indexArray) {
-                    if (itemIndex != int.MaxValue) {
-                        ++value;
-                    }
-                }
-
-                return value;
-            }
-        }
+        public abstract int Count { get; }
 
         public abstract void Add(TKey key, TValue value);
 
@@ -55,105 +40,33 @@ namespace ParksComputing.SetAssociativeCache {
             Add(item.Key, item.Value);
         }
 
-        public bool ContainsKey(TKey key) {
-            var set = FindSet(key);
-            var setBegin = set * Ways;
-            
-            for (int setOffset = 0, offsetIndex = setBegin; setOffset < Ways; ++setOffset, ++offsetIndex) {
-                int itemIndex = indexArray[offsetIndex];
+        public abstract bool ContainsKey(TKey key);
 
-                if (itemIndex != int.MaxValue && 
-                    itemArray[itemIndex].Key.Equals(key)) {
-                    RotateSet(set, setOffset);
-                    return true;
-                }
-            }
+        public abstract bool Contains(KeyValuePair<TKey, TValue> item);
 
-            return false;
-        }
+        public abstract bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value);
 
-        public bool Contains(KeyValuePair<TKey, TValue> item) {
-            var set = FindSet(item.Key);
-            var setBegin = set * Ways;
+        public abstract bool Remove(TKey key);
 
-            for (int setOffset = 0, offsetIndex = setBegin; setOffset < Ways; ++setOffset, ++offsetIndex) {
-                int itemIndex = indexArray[offsetIndex];
+        public abstract bool Remove(KeyValuePair<TKey, TValue> item);
 
-                if (itemIndex != int.MaxValue && 
-                    itemArray[itemIndex].Key.Equals(item.Key) &&
-                    itemArray[itemIndex].Value.Equals(item.Value)) {
-                    RotateSet(set, setOffset);
-                    return true;
-                }
-            }
+        public abstract ICollection<TKey> Keys { get; }
+        public abstract ICollection<TValue> Values { get; }
 
-            return false;
-        }
+        public abstract void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex);
 
-        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value) {
-            var set = FindSet(key);
-            var setBegin = set * Ways;
+        public abstract IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator();
 
-            for (int setOffset = 0, offsetIndex = setBegin; setOffset < Ways; ++setOffset, ++offsetIndex) {
-                int itemIndex = indexArray[offsetIndex];
+        public abstract bool IsReadOnly { get; }
 
-                if (itemIndex != int.MaxValue && itemArray[itemIndex].Key.Equals(key)) {
-                    RotateSet(set, setOffset);
-                    value = itemArray[itemIndex].Value;
-                    return true;
-                }
-            }
-
-            value = default(TValue);
-            return false;
-        }
-
-        public bool Remove(TKey key) {
-            throw new NotImplementedException();
-        }
-
-        public bool Remove(KeyValuePair<TKey, TValue> item) {
-            throw new NotImplementedException();
-        }
-
-        public void Clear() {
-            throw new NotImplementedException();
-        }
-
-        public ICollection<TKey> Keys { get; }
-        public ICollection<TValue> Values { get; }
-        public bool IsReadOnly { get; }
-
-        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() {
-            throw new NotImplementedException();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator() {
-            throw new NotImplementedException();
-        }
+        public abstract void Clear();
 
         protected int FindSet(TKey key) {
             return key.GetHashCode() % Sets;
         }
 
-        protected int RotateSet(int set, int offset) {
-            int retVal = int.MaxValue;
-            int setStart = set * Ways;
-            int headItem = indexArray[setStart + offset];
-
-            System.Array.Copy(indexArray, setStart, indexArray, setStart + 1, offset);
-            indexArray[setStart] = headItem;
-
-            return retVal;
-        }
-
-        protected void Add(TKey key, TValue value, int set, int setOffset, int itemIndex) {
-            itemArray[itemIndex] = KeyValuePair.Create(key, value);
-            RotateSet(set, setOffset);
+        IEnumerator IEnumerable.GetEnumerator() {
+            throw new NotImplementedException();
         }
 
     }
--- LfuArrayCache.cs
+++ LfuArrayCache.cs
@@ -16,10 +16,13 @@ namespace ParksComputing.SetAssociativeCache {
     class LfuArrayCacheImpl<TKey, TValue> : ArrayCacheImplBase<TKey, TValue> {
 
         public LfuArrayCacheImpl(int sets, int ways) : base(sets, ways) {
+            indexArray = new int[Capacity];
+            Array.Fill(indexArray, int.MaxValue);
         }
 
+        protected int[] indexArray;
+
         override public void Add(TKey key, TValue value) {
-            /* This is still the LRU algorithm. I need to replace this with the LFU version. */
             var set = FindSet(key);
             var setBegin = set * Ways;
             int setOffset;
@@ -36,7 +39,7 @@ namespace ParksComputing.SetAssociativeCache {
                     return;
                 }
 
-                if (itemArray[itemIndex].Key.Equals(key)) {
+                if (ItemArray[itemIndex].Key.Equals(key)) {
                     itemIndex = indexArray[offsetIndex];
                     Add(key, value, set, setOffset, itemIndex);
                     return;
@@ -50,6 +53,179 @@ namespace ParksComputing.SetAssociativeCache {
             itemIndex = indexArray[offsetIndex];
             Add(key, value, set, setOffset, itemIndex);
             return;
+        }
+
+        public override int Count {
+            get {
+                int value = 0;
+
+                foreach (int itemIndex in indexArray) {
+                    if (itemIndex != int.MaxValue) {
+                        ++value;
+                    }
+                }
+
+                return value;
+            }
+        }
+
+        public override bool ContainsKey(TKey key) {
+            var set = FindSet(key);
+            var setBegin = set * Ways;
+
+            for (int setOffset = 0, offsetIndex = setBegin; setOffset < Ways; ++setOffset, ++offsetIndex) {
+                int itemIndex = indexArray[offsetIndex];
+
+                if (itemIndex != int.MaxValue &&
+                    ItemArray[itemIndex].Key.Equals(key)) {
+                    UpdateSet(set, setOffset);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public override bool Contains(KeyValuePair<TKey, TValue> item) {
+            var set = FindSet(item.Key);
+            var setBegin = set * Ways;
+
+            for (int setOffset = 0, offsetIndex = setBegin; setOffset < Ways; ++setOffset, ++offsetIndex) {
+                int itemIndex = indexArray[offsetIndex];
+
+                if (itemIndex != int.MaxValue &&
+                    ItemArray[itemIndex].Key.Equals(item.Key) &&
+                    ItemArray[itemIndex].Value.Equals(item.Value)) {
+                    UpdateSet(set, setOffset);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public override bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value) {
+            var set = FindSet(key);
+            var setBegin = set * Ways;
+
+            for (int setOffset = 0, offsetIndex = setBegin; setOffset < Ways; ++setOffset, ++offsetIndex) {
+                int itemIndex = indexArray[offsetIndex];
+
+                if (itemIndex != int.MaxValue && ItemArray[itemIndex].Key.Equals(key)) {
+                    UpdateSet(set, setOffset);
+                    value = ItemArray[itemIndex].Value;
+                    return true;
+                }
+            }
+
+            value = default(TValue);
+            return false;
+        }
+
+        public override bool Remove(TKey key) {
+            var set = FindSet(key);
+            var setBegin = set * Ways;
+
+            for (int setOffset = 0, offsetIndex = setBegin; setOffset < Ways; ++setOffset, ++offsetIndex) {
+                int itemIndex = indexArray[offsetIndex];
+
+                if (itemIndex != int.MaxValue && ItemArray[itemIndex].Key.Equals(key)) {
+                    /* Since all access to the cache values goes through the index array first, 
+                    I'll try leaving the value in the cache and see how that goes. It's faster, 
+                    but for some reason it make me nervous. I suppose I could make value replacement 
+                    a feature of the policy class. */
+                    indexArray[offsetIndex] = int.MaxValue;
+                    UpdateSet(set, setOffset);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public override bool Remove(KeyValuePair<TKey, TValue> item) {
+            var set = FindSet(item.Key);
+            var setBegin = set * Ways;
+
+            for (int setOffset = 0, offsetIndex = setBegin; setOffset < Ways; ++setOffset, ++offsetIndex) {
+                int itemIndex = indexArray[offsetIndex];
+
+                if (itemIndex != int.MaxValue &&
+                    ItemArray[itemIndex].Key.Equals(item.Key) &&
+                    ItemArray[itemIndex].Value.Equals(item.Value)) {
+                    /* Since all access to the cache values goes through the index array first, 
+                    I'll try leaving the value in the cache and see how that goes. It's faster, 
+                    but for some reason it make me nervous. I suppose I could make value replacement 
+                    a feature of the policy class. */
+                    indexArray[offsetIndex] = int.MaxValue;
+                    UpdateSet(set, setOffset);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public override ICollection<TKey> Keys {
+            get {
+                List<TKey> value = new();
+
+                foreach (int itemIndex in indexArray) {
+                    if (itemIndex != int.MaxValue) {
+                        value.Add(ItemArray[itemIndex].Key);
+                    }
+                }
+
+                return value;
+            }
+        }
+        public override ICollection<TValue> Values {
+            get {
+                List<TValue> value = new();
+
+                foreach (int itemIndex in indexArray) {
+                    if (itemIndex != int.MaxValue) {
+                        value.Add(ItemArray[itemIndex].Value);
+                    }
+                }
+
+                return value;
+            }
+        }
+
+        public override void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) {
+            foreach (int itemIndex in indexArray) {
+                if (itemIndex != int.MaxValue) {
+                    array[arrayIndex] = ItemArray[itemIndex];
+                    ++arrayIndex;
+                }
+            }
+        }
+
+        public override IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsReadOnly { get => false; }
+
+        public override void Clear() {
+            throw new NotImplementedException();
+        }
+
+        protected int UpdateSet(int set, int offset) {
+            int retVal = int.MaxValue;
+            int setStart = set * Ways;
+            int headItem = indexArray[setStart + offset];
+
+            System.Array.Copy(indexArray, setStart, indexArray, setStart + 1, offset);
+            indexArray[setStart] = headItem;
+
+            return retVal;
+        }
+
+        protected void Add(TKey key, TValue value, int set, int setOffset, int itemIndex) {
+            ItemArray[itemIndex] = KeyValuePair.Create(key, value);
+            UpdateSet(set, setOffset);
         }
 
     }
