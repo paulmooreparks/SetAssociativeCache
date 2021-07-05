2af7cd85e0ad5fb8dc094321d9d3b38bd79170bc
Paul M. Parks <paul@smartsam.com>
2021-07-05 13:08:19 +0800

    Added comment acknowledging the woeful state of test coverage.

--- SetAssociativeCacheTests/Tests.cs
+++ SetAssociativeCacheTests/Tests.cs
@@ -3,30 +3,71 @@ using NUnit.Framework;
 using ParksComputing.SetAssociativeCache;
 
 namespace SetAssociativeCacheTests {
+    /*
+    This is not an exhaustive set of tests yet. I'll add more when I get some time to loop back around to this project.
+    */
     public class Tests {
         [SetUp]
         public void Setup() {
         }
 
         [Test]
-        public void Test1() {
-            var cache1 = new LruArrayCache<int, int>(16, 4);
-            Assert.IsTrue(cache1.Capacity == (16 * 4));
-
-            var cache2 = new LfuArrayCache<char, string>(6, 6);
-            Assert.IsTrue(cache2.Capacity == (6 * 6));
+        public void LruTest1() {
+            var cache = new LruArrayCache<int, int>(16, 4);
+            Assert.IsTrue(cache.Capacity == (16 * 4));
+            Assert.IsTrue(cache.Count == 0);
+            Assert.IsFalse(cache.TryGetValue(123, out int value));
         }
 
         [Test]
-        public void Test2() {
-            var cache1 = new LruArrayCache<int, int>(4, 4);
-            cache1.Add(38, 9);
-            Assert.IsTrue(cache1.Count == 1);
-            cache1.Add(34, 123);
-            Assert.IsTrue(cache1.Count == 2);
-            Assert.IsTrue(cache1.ContainsKey(38));
-            Assert.IsTrue(cache1.ContainsKey(34));
-            Assert.IsFalse(cache1.ContainsKey(88));
+        public void LruTest2() {
+            var cache = new LruArrayCache<string, string>(6, 6);
+            Assert.IsTrue(cache.Capacity == (6 * 6));
+            Assert.IsTrue(cache.Count == 0);
+            Assert.IsFalse(cache.TryGetValue("abc", out string value));
         }
+
+        [Test]
+        public void LruTest3() {
+            var cache = new LruArrayCache<int, int>(4, 4);
+            cache.Add(38, 9);
+            Assert.IsTrue(cache.Count == 1);
+            cache.Add(34, 123);
+            Assert.IsTrue(cache.Count == 2);
+            Assert.IsTrue(cache.ContainsKey(38));
+            Assert.IsTrue(cache.ContainsKey(34));
+            Assert.IsFalse(cache.ContainsKey(88));
+        }
+
+
+
+        [Test]
+        public void LfuTest1() {
+            var cache = new LfuArrayCache<int, int>(16, 4);
+            Assert.IsTrue(cache.Capacity == (16 * 4));
+            Assert.IsTrue(cache.Count == 0);
+            Assert.IsFalse(cache.TryGetValue(123, out int value));
+        }
+
+        [Test]
+        public void LfuTest2() {
+            var cache = new LfuArrayCache<string, string>(6, 6);
+            Assert.IsTrue(cache.Capacity == (6 * 6));
+            Assert.IsTrue(cache.Count == 0);
+            Assert.IsFalse(cache.TryGetValue("abc", out string value));
+        }
+
+        [Test]
+        public void LfuTest3() {
+            var cache = new LfuArrayCache<int, int>(4, 4);
+            cache.Add(38, 9);
+            Assert.IsTrue(cache.Count == 1);
+            cache.Add(34, 123);
+            Assert.IsTrue(cache.Count == 2);
+            Assert.IsTrue(cache.ContainsKey(38));
+            Assert.IsTrue(cache.ContainsKey(34));
+            Assert.IsFalse(cache.ContainsKey(88));
+        }
+
     }
 }
